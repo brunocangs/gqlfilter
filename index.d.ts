81847d9f4783fab493947bf583f3edcd9093721a
Bruno Cangussu <bruno.cangussu@hotmail.com>
2021-09-09 14:08:01 -0300

    Updated type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module "bc-graphql-filter" {
   export default function gqlFilter<T>(obj: object, filter: string): T;
-  export function bulkFilter<T, U>(obj: U, filter: { [I in keyof U]: U[I] });
+  export function bulkFilter<T, U>(obj: U, filter: { [I in keyof U]: string });
 }
